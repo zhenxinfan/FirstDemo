704feac528b9d54efa10e0b0c03806b7c89bcb96
moyuyu <ganzhenxin1997@gamil.com>
2025-02-28 06:30:55 +0800

    commit test

--- day01/src/main/java/controller/Test01.java
+++ day01/src/main/java/controller/Test01.java
@@ -6,5 +6,11 @@ public class Test01 {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
     }
 }
